824a6efdae3cf767d98939583047ca366fd60490
Luke Myers <luke.myers@knights.ucf.edu>
2016-11-03 00:06:36 -0400

    Created header file to hold the definitions of macros for each instruction op and the M values for the OPR instruction.

---
+++ virtual_machine/opcodes.h
@@ -0,0 +1,36 @@
+//
+// Created by Luke on 11/2/2016.
+//
+
+#ifndef SUPER_DOLLOP_OPCODES_H
+#define SUPER_DOLLOP_OPCODES_H
+
+//op values
+#define LIT 1
+#define OPR 2
+#define LOD 3
+#define STO 4
+#define CAL 5
+#define INC 6
+#define JMP 7
+#define JPC 8
+#define SIO 9
+
+//OPR M values
+#define RET 0
+#define NEG 1
+#define ADD 2
+#define SUB 3
+#define MUL 4
+#define DIV 5
+#define ODD 6
+#define MOD 7
+#define EQL 8
+#define NEQ 9
+#define LSS 10
+#define LEQ 11
+#define GTR 12
+#define GEQ 13
+
+
+#endif //SUPER_DOLLOP_OPCODES_H
