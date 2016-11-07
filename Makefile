2c0bee47048fff532b8cd6a3a6fb20cd938570c0
JonGuilbe <jguilbe@knights.ucf.edu>
2016-11-06 22:16:26 -0500

    Tested command line arguments on Eustis to make sure it actually works this time. Makefile updated and tested with all current c files in the project.

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC = gcc
 
-TARGET = lexer
-CFILES = lexer.c input-output.c symbol-detection.c
+TARGET = compiler
+CFILES = compiler.c ./lexer/input_output.c ./lexer/lexer.c ./lexer/symbol_detection.c ./parser/parse.c ./parser/symbols.c ./virtual_machine/input_output.c ./virtual_machine/instructions.c ./virtual_machine/util.c ./virtual_machine/vm.c
 
 all: $(TARGET)
 
--- compiler.c
+++ compiler.c
@@ -15,7 +15,6 @@ int main(int argc, char **argv) {
     MainArguments *arguments = parseArguments(argc, argv);
 
     // Check to see if a file path wasn't passed via command line
-    // Probably shouldn't be needed anymore
     if(isEmptyPathError(arguments->inputPath) || isEmptyPathError(arguments->outputPath)){
         printEmptyPathError();
         exit(-1);
@@ -60,14 +59,11 @@ int main(int argc, char **argv) {
 MainArguments* parseArguments(int argc, char **argv){
     // Pull out the command line arguments
     MainArguments* arguments = calloc(1, sizeof(MainArguments));
-    if(argc != 2) {
-        printf("Error: Input Mismatch");
+    if(argc != 3) {
+        printf("Error: Input Mismatch\n");
         exit(-1);
     }
-    if (strncmp(argv[0], "--", 2) == 1 && strncmp(argv[1], "--", 2) == 1 ) {
-        arguments->inputPath = argv[0];}
-    else{
-        printf("Error: Input Mismatch");
-        exit(-1);}
+    arguments->inputPath = argv[1];
+    arguments->outputPath = argv[2];
     return arguments;
 }
